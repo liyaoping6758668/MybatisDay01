71a29d398f77543f3cf289ad45b325a14a7951ce
liyaoping <liyaoping@qq.com>
2020-01-21 21:34:24 +0800

    mybatis使用一对一方式

---
+++ src/main/java/com/ping/dao/AccountUserDao.java
@@ -0,0 +1,17 @@
+package com.ping.dao;
+
+import com.ping.domain.AccountUser;
+
+import java.util.List;
+
+/**
+ * @author:lyp
+ * @date 2020/1/21-21:25
+ */
+public interface AccountUserDao {
+    /**
+     * 查询所有账号信息对应的用户名和地址
+     * @return
+     */
+    List<AccountUser> findAll();
+}
